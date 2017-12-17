3ea50b71140265009d04d27be9996732ab4bf0bd
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-12-17 16:23:10 +0600

    first

---
+++ InterfacesHomeWork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            //{
+                
+
+                WriteLine("Введите имя студента: ");
+                string name = ReadLine();
+
+                WriteLine("Введите полное имя студента: ");
+                string fullN = ReadLine();
+
+                WriteLine("Поставьте оценки студету: ");
+                int[] grades = new int[int.Parse(ReadLine())];
+                // for(int i=0; i<)
+
+                Student student = new Student(name, fullN, grades);
+               // Clear();
+
+                WriteLine("Имя: " + student.GetName());
+                WriteLine("Полное имя: " + student.GetFullName());
+                for (int i = 0; i < grades.Length; i++)
+                {
+                    WriteLine("Оценкa: " + grades[i]);
+                }
+
+                ReadLine();
+                Clear();
+            //}
+        }
+    }
+}
---
+++ InterfacesHomeWork/Student.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesHomeWork
+{
+    public class Student : IStudent
+    {
+        private string name;
+        private string fullName;
+        int i = 10;
+        //int[] grades = new int[5];
+
+        public Student(string name_, string fullName_, int[] grades) 
+        {
+            Name = name_;
+            FullName = fullName_;
+            Grades = grades;
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+
+            set
+            {
+                fullName = value;
+            }
+        }
+
+        public int[] Grades
+        {
+            get
+            {
+                return Grades;
+            }
+
+            set
+            {
+
+                Grades = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double GetAvgGrade()
+        {
+            int sum = 0;
+            for(int i=0; i<Grades.Length; i++)
+            {
+                sum += Grades[i];
+            }
+            return sum;
+        }
+
+        public string GetFullName()
+        {
+            return FullName;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
